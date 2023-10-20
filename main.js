9e64b5153543bc984a74506f1d897ec7affea408
priyankacute <bhavanimurugesh2000@gmail.com>
2023-10-20 18:48:16 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,12 @@
+let num=prompt("enter the number");
+
+let number=(num)=>{
+    if(num%2==0){
+        document.write("the given number is even")
+    }
+    else{
+        document.write("the given number is")
+    }
+        }
+    
+number(num)
